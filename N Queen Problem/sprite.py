7d24d162ad21b41580f09b00ac6b3b363a2c7048
magoninho <magoninhogamer@gmail.com>
2024-11-25 10:36:33 -0300

    sprite antialiasing

--- N Queen Problem/sprite.py
+++ N Queen Problem/sprite.py
@@ -6,7 +6,7 @@ class Sprite(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
     
         self.image = pygame.image.load("queen.png").convert_alpha()
-        self.image = pygame.transform.scale(self.image, (width, height))
+        self.image = pygame.transform.smoothscale(self.image, (width, height))
 
         self.rect = self.image.get_rect()
     
